c6c5a2b2af3bf2e21761511e271515173aa053f3
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-04-11 14:36:30 -0400

    Create variablesAndOperations.py

---
+++ Python_Crash_Course/variablesAndOperations.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+print("Hello World")
+
+a = 12
+b = "THis is a string"
+c = 12.5
+
+print(a,b,c)
+
+#mathematical Operation
+
+num1 = 12
+num2 = 4
+
+result = num1 + num2
+result = num1 - num2
+result = num1 * num2
+result = num1 / num2
+
+print(result)
+
+sen = "i love avengers \n"
+print(sen)
+
+print("hello", end=" ")
+print("World")
