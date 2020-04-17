e4b057cd9cbef267a1c20a791fc49e8c56b89870
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-04-17 14:37:57 -0400

    Create regex5.py

---
+++ regex/regex5.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+
+X = ["This is a wolf @scary",
+     "Welcome to the jungle #missing",
+     "111322 the number to know",
+     "Remember the name s - John",
+     "I                love               you"]
+
+for i in range(len(X)):
+    X[i] = re.sub(r"\W"," ",X[i])
+    X[i] = re.sub(r"\d"," ",X[i])
+    X[i] = re.sub(r"\s+[a-z]\s+"," ",X[i],flags=re.I)
+    X[i] = re.sub(r"\s+"," ",X[i])
+    X[i] = re.sub(r"^\s","",X[i])
+    X[i] = re.sub(r"\s$","",X[i])
+    
+    
+    print(X)
