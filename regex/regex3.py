cc084ae6d08ae8fb5c98c17e7144bad3226675c3
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-04-17 14:04:55 -0400

    Update regex3.py

--- regex/regex3.py
+++ regex/regex3.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 
+
+import re
+
+sentence = "I love Avengers Avengers"
+
+print(re.sub(r"Avengers", "Justice League", sentence)) #replace the pattern 
+
+print(re.sub(r"[a-z", "0", sentence,1, flags=re.I)) # the count 1 is to match and replace that many characters
