65f564e157a620de6971538fcc0b4de60a787d1d
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-04-17 19:32:22 -0400

    update NLTK

---
+++ NLTK/nlp18.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from nltk.corpus import wordnet
+
+# Initializing the list of synnonyms and antonyms
+synonyms = []
+antonyms = []
+
+for syn in wordnet.synsets("good"):
+    for s in syn.lemmas():
+        synonyms.append(s.name())
+        for a in s.antonyms():
+            antonyms.append(a.name())
+            
+            
+# Displaying the synonyms and antonyms
+print(set(synonyms))
+print()
+print(set(antonyms))
