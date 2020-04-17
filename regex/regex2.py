e0a3840a41c9ef385bab7a3beae4ae4aef7d4469
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-04-17 12:47:54 -0400

    Update regex2.py

--- regex/regex2.py
+++ regex/regex2.py
@@ -1,2 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import re
+
+sentence ="1996 was born in the I year born"
+
+print(re.match(r"[a-zA-Z]+",sentence)) # IT ONLY SEARCHES The fiirst element iin teh sentence
+print(re.search(r"[a-zA-Z]+",sentence))
+
+
+#starts with 
+
+if re.match(r"1996", sentence):
+    print("match")
+else:
+    print("unmatch")
+
+
+#ends with 
+    
+if re.search(r"born$", sentence): #dollar symbol is end of sentence
+    print("match")
+else:
+    print("unmatch")
